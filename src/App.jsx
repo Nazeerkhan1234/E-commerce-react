aa0935292ecded0cf51fb448ff0117fccd13d312
Nazeerkhan1234 <nazeerkhan9695@gmail.com>
2024-09-06 11:13:03 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shop from "./pages/Shop";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import LoginSignup from "./pages/LoginSignup";
+import ShopCategory from "./pages/ShopCategory";
 function App() {
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory category="mens" />} />
+          <Route path="/womens" element={<ShopCategory category="womens" />} />
+          <Route path="/kids" element={<ShopCategory category="kids" />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/Shop.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../components/hero/Hero";
+import Popular from "../components/popular/Popular";
+
+const Shop = () => {
+  return (
+    <>
+      <Hero />
+      <Popular />
+    </>
+  );
+};
+
+export default Shop;
